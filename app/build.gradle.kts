ff6dfe9642be479cd6bed775573094be7e58a230
beeric1 <40263439+beeric1@users.noreply.github.com>
2026-05-19 23:02:54 +0200

    release v1.3.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.haertibraeu.hopledger"
         minSdk = 29
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.3.14"
+        versionCode = 18
+        versionName = "1.3.15"
     }
 
     buildTypes {
